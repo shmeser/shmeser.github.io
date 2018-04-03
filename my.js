2e3b1ba382e97b1d3b544f513373db36fd0e8d20
shmeser <shmeser-rs13@mail.ru>
2018-04-03 12:27:09 +0300

    index

--- my.js
+++ my.js
@@ -205,16 +205,23 @@ $(document).ready(function() {
 
 $(window).load(function() {
 
-    function onCreated(node) {
-      console.log(node);
-    }
-
-    var createBookmark = browser.bookmarks.create({
-      title: "bookmarks.create() on MDN",
-      url: "https://developer.mozilla.org/Add-ons/WebExtensions/API/bookmarks/create"
+    
+    $(function() {
+      $('#bookmarkme').click(function() {
+        if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+          window.sidebar.addPanel(document.title, window.location.href, '');
+        } else if (window.external && ('AddFavorite' in window.external)) { // IE Favorite
+          window.external.AddFavorite(location.href, document.title);
+        } else if (window.opera && window.print) { // Opera Hotlist
+          this.title = document.title;
+          return true;
+        } else { // webkit - safari/chrome
+          alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+        }
+      });
     });
+    
 
-    createBookmark.then(onCreated);
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js');
     }
