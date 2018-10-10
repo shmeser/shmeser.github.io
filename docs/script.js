c3feed86683771a9c56791be1b02db2b70a1d063
shmeser <shmeser.regium@gmail.com>
2018-10-10 11:32:21 +0300

    fx

--- docs/script.js
+++ docs/script.js
@@ -15,16 +15,20 @@ new Vue({
     mounted() {
         axios({
             method: 'post',
-            contentType: 'application/x-www-form-urlencoded',
+//            contentType: 'application/x-www-form-urlencoded',
             url: 'https://api.admitad.com/token/',
-            data: 'grant_type=client_credentials&client_id=cb281d918a37e346b45e9aea1c6eb7&scope=advcampaigns banners websites'
+            data: 'code=87eaa2f4587345bad56c99ccdd71cb&client_secret=202aa3513778f233c63bd5c572e889&grant_type=authorization_code&client_id=a3205734413fcbef29ee6f18945716&redirect_uri=https%3A%2F%2Fgoogle.com%2F'
 //                    {
 //                grant_type: 'client_credentials',
-//                client_id: 'cb281d918a37e346b45e9aea1c6eb7',
+//                client_id: 'a3205734413fcbef29ee6f18945716',
 //                scope: 'advcampaigns banners websites'
-//            }
             ,
-            config: {headers: {'Authorization': 'Basic YTMyMDU3MzQ0MTNmY2JlZjI5ZWU2ZjE4OTQ1NzE2OjIwMmFhMzUxMzc3OGYyMzNjNjNiZDVjNTcyZTg4OQ=='}}
+            config: {
+                headers: {
+                    'Authorization': 'Basic YTMyMDU3MzQ0MTNmY2JlZjI5ZWU2ZjE4OTQ1NzE2OjIwMmFhMzUxMzc3OGYyMzNjNjNiZDVjNTcyZTg4OQ==',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
         }).then(response => {
             this.info = response;
         }).catch(error => {
