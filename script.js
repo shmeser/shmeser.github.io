9587133eedd186e4f2d18530f30f36b201b36fd5
shmeser <shmeser.regium@gmail.com>
2018-10-10 13:41:33 +0300

    x

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ new Vue({
     },
     mounted() {
         axios({
-            method: 'post',
+            method: "POST",
             url: 'https://api.admitad.com/token/',
             data: 'code=2124ca07a8230a01caf675ccd87716&client_secret=202aa3513778f233c63bd5c572e889&grant_type=authorization_code&client_id=a3205734413fcbef29ee6f18945716&redirect_uri=https%3A%2F%2Fgoogle.com%2F'
-            ,
-            config: {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            }
+//            ,
+//            config: {
+//                headers: {
+//                    'Content-Type': 'application/x-www-form-urlencoded'
+//                }
+//            }
         }).then(response => {
             this.info = response;
         }).catch(error => {
